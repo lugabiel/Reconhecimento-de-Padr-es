49d33ad78325043e2d7900938742ee1827797cbf
lugabiel <36977837+lugabiel@users.noreply.github.com>
2021-01-11 01:26:14 -0300

    add-jupyter-notebook & more
    Added some modularization & jupyter notebook template

--- exercicio1-gassian-data.py
+++ exercicio1-gassian-data.py
@@ -1,3 +1,7 @@
+# <><> Gabriel Aragão - 2021 <><>
+# TODO: a class for a dataset(std deviation,mean)
+#     : 
+
 # importing libraries
 
 import math as mt
@@ -10,21 +14,34 @@ from mpl_toolkits.mplot3d import Axes3D
 from matplotlib.ticker import LinearLocator, FormatStrFormatter
 
 
-# funcao estimativa da densidade de 2 variaveis
-
+# funcao densidade de probabilidade para 2 variaveis
 def pdf2var(x,y,u1,u2,s1,s2,p):
     # x ,y : indices do ponto no espaco R^2
     # u1,u2: media media de cada variavel no dataset 
     # s1,s2: desvio padrao de cada variavel no dataset
-    
+    # p    : coeficiente de correlacao 
     A = (1/(2*mt.pi*s1*s2*np.sqrt(1-(p**2))))
     B = ((-(1)/(2*(1-(p**2)))))
     C = ((x-u1)**2)/((s1**2))
     D = (((y-u2)**2)/((s2)**2))
     E = ((-2*p*(x-u1)*(y-u2))/(s1*s2)) #termo de covariancia
-    
     return A*mt.exp(B*(C+D+E))
     #return (1/(2*mt.pi*s1*s2*np.sqrt(1-(p**2))))*mt.exp(((-(1)/(2*(1-(p**2)))))*((((x-u1)**2)/((s1**2))+(((y-u2)**2)/((s2)**2))-((2*p*(x-u1)*(y-u2))/(s1*s2)))))
+
+# mesuring probability surface for both datasets
+def genSurface(mgenA,dgenA,mgenB,dgenB,correlacao,M1,M2):
+    ci = 0
+    for i in seqi:
+        ci = ci + 1
+        cj = 0
+        for j in seqj:
+            cj = cj + 1
+            aux  = pdf2var(i,j,mgenA[0],mgenA[1],dgenA[0],dgenA[1],correlacao)
+            aux2 = pdf2var(i,j,mgenB[0],mgenB[1],dgenB[0],dgenB[1],correlacao)
+            #print(ci,cj,'--', aux, aux2)
+            M1[0][ci-1][cj-1] = aux
+            M2[0][ci-1][cj-1] = aux2
+    return M1,M2
 
 # defining mean and standard deviation
 mu = 0.5
@@ -59,22 +76,14 @@ X,Y = np.meshgrid(seqi,seqj)
 
 M1 = [np.zeros([len(seqi),len(seqj)])]
 M2 = [np.zeros([len(seqi),len(seqj)])]
-correlacao = 0.1
+correlacao = 0.0
+#steps = 10
+#for steps in steps:
 
 # mesuring probability surface for both datasets
-ci = 0
-for i in seqi:
-    ci = ci + 1
-    cj = 0
-    for j in seqj:
-        cj = cj + 1
-        aux  = pdf2var(i,j,mgenA[0],mgenA[1],dgenA[0],dgenA[1],correlacao)
-        aux2 = pdf2var(i,j,mgenB[0],mgenB[1],dgenB[0],dgenB[1],correlacao)
-        #print(ci,cj,'--', aux, aux2)
-        M1[0][ci-1][cj-1] = aux
-        M2[0][ci-1][cj-1] = aux2
+M1, M2 = genSurface(mgenA,dgenA,mgenB,dgenB,correlacao, M1, M2)
 
-#  gen dataset 
+#  gen dataset A & B
 plt.scatter(xgenA,ygenA, color = 'r',marker =".")
 plt.scatter(xgenB,ygenB, color = 'g',marker ='.')
 
