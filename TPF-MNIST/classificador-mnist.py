8becb585ff4ad5f409423f5cccc1a414888a6f62
lugabiel <36977837+lugabiel@users.noreply.github.com>
2021-03-16 02:30:57 -0300

    identificando classes
    enumerando rótulos (classes) presentes no dataset de treinamento.

--- TPF-MNIST/classificador-mnist.py
+++ TPF-MNIST/classificador-mnist.py
@@ -1,13 +1,21 @@
 import cv2
 import csv
+import time as t
+
+tInicial = t.time()
 
 trainData = ''
 line = ''
 
 with open('trainReduzido.csv', newline ='\n') as trainCsv:
     trainData = csv.reader(trainCsv,delimiter=' ')
+    lastline = -1
     for line in trainData:
-        print(line)
+        if (line[0][0] != lastline):
+            lastline = line[0][0]
+            print(line)
     
 
 
+tFinal = t.time()
+print(tFinal - tInicial)
